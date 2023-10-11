7ecf5b860a5d101a6eeb3859d38ba49a5f50c97d
Coument <qaqaqa282562@gmail.com>
2023-10-12 01:21:15 +0800

    hw2_10/12

---
+++ src/hw2/hw2.java
@@ -0,0 +1,83 @@
+package hw2;
+public class hw2{
+	public static void main(String[]args){
+		/*System.out.print("歡迎使用java");
+		System.out.print("開始使用java吧");*/
+		
+		/*System.out.println("歡迎使用java");
+		System.out.println("開始使用java吧");*/
+		
+		/*System.out.println('B');
+		System.out.println(123);*/
+		
+		/*System.out.println("java\\");
+		System.out.println("java\'");*/
+		
+		/*System.out.println("8進位101=\101");
+		System.out.println("16進位0061=\u0061");*/
+		
+		/*int num,num2;
+		num = 3;
+		num2 = 6;
+		System.out.println("num的值是"+num);
+		num = 5;
+		System.out.println("更新後num的值是"+num);
+		num = num2;
+		System.out.println("二次更新後num的值是"+num);*/
+		
+		/*System.out.println("請輸入一個整數:");
+		BufferedReader br1 = 
+				new BufferedReader(new InputStreamReader(System.in));
+		String str1 = br1.readLine();
+		int num = Integer.parseInt(str1);
+		System.out.println("您輸入的數字是:"+num);
+		System.out.println("請輸入字串");
+		BufferedReader br2 = 
+				new BufferedReader(new InputStreamReader(System.in));
+		String str2 = br2.readLine();
+		System.out.println("您輸入的字串是:"+str2);*/
+		
+		/*System.out.println("5-3="+(5-3));
+		System.out.println("5*3="+(5*3));
+		int num1 = 1;
+		int num2 = 2;
+		int sum = num1 + num2;
+		System.out.println("num1="+num1);
+		System.out.println("num2="+num2);
+		System.out.println("num1 + num2 ="+sum);
+		num1 = num1 + 1;
+		System.out.println("num1 +1 ="+num1);*/
+		
+		/*int num1 = 20;
+		int num2 = 10;
+		System.out.println("num1 = "+num1);
+		System.out.println("num2 = "+num2);
+		System.out.println("num1 & num2 的各種運算");
+		System.out.println("num1 + num2 = "+(num1+num2));
+		System.out.println("num1 - num2 = "+(num1-num2));
+		System.out.println("num1 * num2 = "+(num1*num2));
+		System.out.println("num1 / num2 = "+(num1/num2));
+		System.out.println("num1 % num2 = "+(num1%num2));
+		int a=0,b=0,c=0;
+		b = a++;
+		c = ++a;
+		System.out.println("B的值為"+b);
+		System.out.println("C的值為"+c);*/
+		
+		/*double d_num = 170.5;
+		System.out.println("身高是"+d_num+"公分");
+		System.out.println("指定給int型態的整數");
+		int inum = (int)d_num;
+		System.out.println("身高是"+inum+"公分");*/
+		
+		/*int d = 4;
+		double pi =3.14;
+		System.out.println("直徑為"+d+"公分的圓");
+		System.out.println("其圓周為"+(d*pi)+"公分");
+		int num=5,num1=4;
+		double div1 = num/num1;
+		double div2 = (double)num/(double)num1;
+		System.out.println("5/4等於"+div1);
+		System.out.println("5/4等於"+div2);*/
+	}
+}
